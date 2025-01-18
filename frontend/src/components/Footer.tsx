0933aaef754d69fa3921553d861ef862b8047290
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-01-17 22:58:38 -0700

    added footer and vertical scrolling to the page content section

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+	return (
+		<div className="flex flex-row h-16">
+			<div className="flex-1"></div>
+			<div className="flex-1 flex justify-center items-center">
+				<p>&copy; 2025 Tony Tran</p>
+			</div>
+			<div className="flex-1"></div>
+		</div>
+	);
+};
+
+export default Footer;
--- frontend/src/pages/List.tsx
+++ frontend/src/pages/List.tsx
@@ -1,17 +1,25 @@
+import Footer from '../components/Footer'
 import ListSideBar from '../components/ListSideBar'
 import ListTable from '../components/ListTable'
 
 const List = () => {
   return (
-    <div className='page'>
+    <div className='page h-[calc(100vh-4rem)]'>
       <div className='page-side-bar'>
         <ListSideBar/>
       </div>
       <div className='page-content'>
-        <ListTable/>
+        <div className='flex flex-col min-h-screen'>
+          <div className='px-6 pb-6 bg-purple-500'>
+            <ListTable/>
+          </div>
+          <div className='mt-auto bg-blue-500'>
+            <Footer/>
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
-export default List
+export default List;
