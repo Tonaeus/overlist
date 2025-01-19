bae0c85373fb374e79e9c161f9f29cf5915172f2
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-01-18 20:44:18 -0700

    changed title to a link to home

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
   return (
     <div className='flex flex-row h-16'>
       <div className='flex-1'></div>
       <div className='flex-1 flex justify-center items-center'>
-        <h1 className='text-4xl font-bold'>Overlist</h1>
+        <Link to="/" className="text-4xl font-bold">
+          Overlist
+        </Link>
       </div>
       <div className='flex-1'></div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
