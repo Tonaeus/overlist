7e0f565cb7ab716c3f25a52789d2f4de07862cb7
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-01-19 17:54:08 -0700

    fixed a cors error and added a .env file to the backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,29 @@
+import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
+
 import directoryRoutes from "./routes/directories.js";
 import listRoutes from "./routes/lists.js";
 import listHeaderRoutes from "./routes/listHeaders.js";
 import listBodyRoutes from "./routes/listBodies.js";
 
-const MONGO_URI = 'mongodb://root_username:root_password@localhost:27017/database?authSource=admin'; 
-const PORT = 3000;
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+
+const mongoUri = process.env.MONGO_URI as string;
+const port = process.env.PORT as string;
+const frontendUrl = process.env.FRONTEND_URL as string;
 
 // Express app
 const app = express();
 
 // Middleware
 app.use(express.json());
+
+app.use(cors({
+  origin: frontendUrl, 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+}));
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
@@ -26,11 +37,11 @@ app.use("/api/listHeaders", listHeaderRoutes);
 app.use("/api/listBodies", listBodyRoutes);
 
 // Connect to db
-mongoose.connect(MONGO_URI)
+mongoose.connect(mongoUri)
   .then(() => {
     // Listen for requests
-    app.listen(PORT, () => {
-      console.log('Connected to db and listening on port', PORT)
+    app.listen(port, () => {
+      console.log('Connected to db and listening on port', port)
     });
   })
   .catch((error) => {
