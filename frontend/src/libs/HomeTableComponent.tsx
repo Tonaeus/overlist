bc7f2fe81ba2e3653e3030505a3e67848de1816c
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-03-01 14:13:32 -0700

    made empty message for tables remain in the center

--- frontend/src/libs/HomeTableComponent.tsx
+++ frontend/src/libs/HomeTableComponent.tsx
@@ -1,5 +1,7 @@
 import type { TableComponentProps } from "../types/TableComponent";
 
+import { useEffect, useRef, useState } from "react";
+
 import {
 	Table,
 	Header,
@@ -22,6 +24,26 @@ const HomeTableComponent = ({
 	select,
 	emptyMessage,
 }: TableComponentProps) => {
+	const elementOneRef = useRef<HTMLDivElement | null>(null);
+	const elementTwoRef = useRef<HTMLDivElement | null>(null);
+	const [elementOneWidth, setElementOneWidth] = useState(0);
+
+	useEffect(() => {
+		const handleResize = () => {
+			if (elementOneRef.current) {
+				setElementOneWidth(elementOneRef.current.offsetWidth);
+			}
+		};
+
+		handleResize();
+
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
 	return (
 		<div className="bg-white p-1.5 rounded accent-blue-700 border border-line">
 			<div className="overflow-x-auto">
@@ -60,9 +82,16 @@ const HomeTableComponent = ({
 					)}
 				</Table>
 				{data.nodes.length === 0 ? (
-					<div className="flex justify-center items-center h-9">
-						<p>{emptyMessage}</p>
-					</div>
+					<>
+						<div ref={elementOneRef} className="h-9"></div>
+						<div
+							ref={elementTwoRef}
+							className="fixed flex justify-center items-center h-9 -mt-9"
+							style={{ width: `${elementOneWidth}px` }}
+						>
+							<p>{emptyMessage}</p>
+						</div>
+					</>
 				) : null}
 			</div>
 		</div>
