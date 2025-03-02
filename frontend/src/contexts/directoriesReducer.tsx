98e261d17f00dcf1f7ff0118569f8362fb404b76
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-03-02 16:24:40 -0700

    fixed directories context not updating

--- frontend/src/contexts/directoriesReducer.tsx
+++ frontend/src/contexts/directoriesReducer.tsx
@@ -1,5 +1,7 @@
 import type { DirectoriesState, DirectoriesAction } from "../types/Directory";
 
+import { sortObjectsByProp } from "../utils/sortUtils";
+
 const directoriesReducer = (
 	state: DirectoriesState,
 	action: DirectoriesAction
@@ -8,6 +10,24 @@ const directoriesReducer = (
 		case "SET_DIRECTORIES":
 			return {
 				directories: action.payload,
+			};
+		case "CREATE_DIRECTORY":
+			return {
+				directories: [...state.directories, action.payload].sort(
+					sortObjectsByProp("label")
+				),
+			};
+		case "UPDATE_DIRECTORY":
+			return {
+				directories: state.directories
+					.map((dir) => (dir.id === action.payload.id ? action.payload : dir))
+					.sort(sortObjectsByProp("label")),
+			};
+		case "DELETE_DIRECTORY":
+			return {
+				directories: state.directories.filter(
+					(dir) => dir.id !== action.payload.id
+				),
 			};
 		default:
 			return state;
--- frontend/src/contexts/listColumnsReducer.tsx
+++ frontend/src/contexts/listColumnsReducer.tsx
@@ -1,7 +1,5 @@
 import type { ListColumnsState, ListColumnsAction } from "../types/ListColumn";
 
-import { sortObjectsByProp } from "../utils/sortUtils";
-
 const listColumnsReducer = (
 	state: ListColumnsState,
 	action: ListColumnsAction
@@ -10,24 +8,6 @@ const listColumnsReducer = (
 		case "SET_LIST_COLUMNS":
 			return {
 				listColumns: action.payload,
-			};
-		case "CREATE_LIST_COLUMN":
-			return {
-				listColumns: [...state.listColumns, action.payload].sort(
-					sortObjectsByProp("label")
-				),
-			};
-		case "UPDATE_LIST_COLUMN":
-			return {
-				listColumns: state.listColumns
-					.map((col) => (col.id === action.payload.id ? action.payload : col))
-					.sort(sortObjectsByProp("label")),
-			};
-		case "DELETE_LIST_COLUMN":
-			return {
-				listColumns: state.listColumns.filter(
-					(col) => col.id !== action.payload.id
-				),
 			};
 		default:
 			return state;
