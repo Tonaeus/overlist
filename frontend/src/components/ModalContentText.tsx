37b99b5852348e06514f7c8825ec0d4d2acb9484
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-03-10 00:29:28 -0600

    fixed long message for text modal

--- frontend/src/components/ModalContentText.tsx
+++ frontend/src/components/ModalContentText.tsx
@@ -1,12 +1,14 @@
 type ModalContentTextProps = {
 	message: string;
 	error?: string;
-}
+};
 
 const ModalContentText = ({ message, error }: ModalContentTextProps) => {
 	return (
 		<>
-			<p className={`${error ? "mb-1.5" : ""}`}>{message}</p>
+			<p className={`${error ? "mb-1.5" : ""} break-words hyphens-auto`}>
+				{message}
+			</p>
 			{error && (
 				<div className="w-full min-h-9 border border-red-500 rounded p-1.5 bg-red-100 text-red-500 break-words whitespace-normal">
 					{error}
